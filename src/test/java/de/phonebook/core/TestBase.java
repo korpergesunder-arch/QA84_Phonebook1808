012460e4692f092626c6d997d6c87cc7c4177aac
Yevhenii <stupachenko30@gmail.com>
2026-08-17 12:58:47 +0200

    add 20.1

--- src/test/java/de/phonebook/core/TestBase.java
+++ src/test/java/de/phonebook/core/TestBase.java
@@ -1,24 +1,41 @@
 package de.phonebook.core;
 
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.lang.reflect.Method;
+
 public class TestBase {
 
 
-    protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+    protected static ApplicationManager app =
+            new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite   //запуск в одном окне
-    public void setUp () {
+    public void setUp() {
         app.init();
     }
 
     @AfterSuite(enabled = true)  //не закрывает браузер
-    public void tearDown(){
+    public void tearDown() {
         app.stop();
     }
 
+
+@BeforeMethod
+public void startTest(Method method){
+    logger.info("Start test {}",method.getName() );
 }
+
+@AfterMethod
+public void stopTest(){
+    logger.info("Stop test");
+}}
+
